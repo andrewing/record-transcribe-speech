05e6e7a44a3a47fe9d05b1ca598a2b9b6bd3621d
Andrew Ing <john.andrew.ing22@gmail.com>
2023-03-30 22:40:00 +0800

    feat:added tutorial nav

--- components/CustomNav/index.jsx
+++ components/CustomNav/index.jsx
@@ -1,4 +1,5 @@
-import { Navbar } from "@nextui-org/react"
+import { Navbar, Spacer } from "@nextui-org/react"
+import Image from "next/image"
 import { useEffect } from "react"
 
 const CustomNav = ({ active }) => {
@@ -41,6 +42,15 @@ const CustomNav = ({ active }) => {
                     href="#contact"
                 >
                     Contact
+                </Navbar.Link>
+                <Navbar.Link
+                    id="tutorialnav"
+                    as="a"
+                    href="https://app.tango.us/app/workflow/Recording-Guide---2becfd04cd724662a0f0c0348eb1a635"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    Tutorial <Spacer x={0.3} /><Image src="/newtabsymbol.png" width={12} height={12} />
                 </Navbar.Link>
             </Navbar.Content>
         </Navbar>
